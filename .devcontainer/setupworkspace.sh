a27790fc93c835de3cf094b69e3ad3aab0ae1514
Michiel van Heusden <m.vanheusden@basicit.nl>
2026-02-25 14:10:20 +0100

    Changed path

--- .devcontainer/setupworkspace.sh
+++ .devcontainer/setupworkspace.sh
@@ -1,8 +1,10 @@
 dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
 # Restore unittests
+echo "Restoring UnitTests"
 cd test/UnitTests/
 dotnet restore
 cd ../..
 # Restore Library.ApplicationCore
-cd .. src/Library.ApplicationCore
+echo "Library.ApplicationCore"
+cd src/Library.ApplicationCore
 dotnet restore
